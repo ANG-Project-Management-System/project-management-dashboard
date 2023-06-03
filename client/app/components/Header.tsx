828bbbc1b4996450281f0f7649c253c86b509285
Steven Duong <stevenduongx@gmail.com>
2023-06-02 21:36:26 -0600

    create-next-app

---
+++ client/app/components/Header.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { Button } from "@chakra-ui/react"
+
+const Header = () => {
+  return (
+    <div className="flex w-full items-center flex-1 md:flex-initial">
+        <Button 
+            colorScheme='blue'
+        >
+            Button
+        </Button>
+    </div>
+  )
+}
+export default Header
---
+++ client/app/page.tsx
@@ -0,0 +1,12 @@
+//* Server side component *//
+import Image from 'next/image'
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import Header from './components/Header';
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+    </main>
+  )
+}
