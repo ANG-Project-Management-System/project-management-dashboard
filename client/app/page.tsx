183ee0ccddcefb85b051f79cf7d35165631adc8c
Steven Duong <stevenduongx@gmail.com>
2023-06-02 23:02:11 -0600

    create-sidebar

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,12 +1,12 @@
 //* Server side component *//
 import Image from 'next/image'
 import { Button, ButtonGroup } from '@chakra-ui/react'
-import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 
 export default function Home() {
   return (
     <main>
-      <Header />
+      <Sidebar children={undefined} />
     </main>
   )
 }
