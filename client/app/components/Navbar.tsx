2b9d8c59a7bc353208cd97419f1b3c7b5264c9d4
Steven Duong <stevenduongx@gmail.com>
2023-06-06 19:13:46 -0600

    add-login-function

--- client/app/components/Navbar.tsx
+++ client/app/components/Navbar.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+// Google OAuth imports
+import { useSession } from "next-auth/react";
+import { signOut } from 'next-auth/react';
+
+// React imports
 import { ReactNode, useState } from "react";
 import {
   Box,
@@ -50,6 +55,9 @@ const NavLink = ({ children }: { children: ReactNode }) => (
 export default function Navbar() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [isExpanded, setIsExpanded] = useState(true);
+  const { data: session } = useSession();
+  const defaultName = "Justina Clark";
+  const defaultImageURL = "https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9";
 
   const toggleSidebar = () => {
     setIsExpanded(!isExpanded);
@@ -103,7 +111,9 @@ export default function Navbar() {
                   <Avatar
                     size={"sm"}
                     src={
-                      "https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
+                      session?.user?.image
+                        ? session.user.image
+                        : defaultImageURL
                     }
                   />
                   <VStack
@@ -112,11 +122,14 @@ export default function Navbar() {
                     spacing="1px"
                     ml="2"
                   >
-                    <Text fontSize="sm">Justina Clark</Text>
+                    <Text fontSize="sm">
+                      {session?.user?.name ? session.user.name : defaultName}
+                    </Text>
                     <Text fontSize="xs" color="gray.600">
                       Admin
                     </Text>
                   </VStack>
+
                   <Box display={{ base: "none", md: "flex" }}>
                     <FiChevronDown />
                   </Box>
@@ -126,7 +139,7 @@ export default function Navbar() {
                 <MenuItem>Profile</MenuItem>
                 <MenuItem>Settings</MenuItem>
                 <MenuDivider />
-                <MenuItem>Log out</MenuItem>
+                <MenuItem onClick={() => signOut()}>Log out</MenuItem>
               </MenuList>
             </Menu>
           </Flex>
